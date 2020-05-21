4a5b45a2528392b20ee38567a8be60f6b319b7fc
Lu lan <llan@bandwidth.com>
2020-05-21 11:47:27 -0400

    Using the SDK, since it hasn't been publish, pulling from local drive

--- src/bwTwoFactor.js
+++ src/bwTwoFactor.js
@@ -1,57 +1,28 @@
-const axios = require('axios');
+const mfa = require('@bandwidth/mfa');
+
+// Configure the library to use the correct api credentials
+mfa.Configuration.basicAuthUserName = process.env.BW_API_USER;
+mfa.Configuration.basicAuthPassword = process.env.BW_API_PASS;
 
 const requestTwoFactorPin = async () => {
-  axios
-    .post(process.env.BW_TWOFACTOR_URL, {
-      Text2FA: [{
-        AccountId: [process.env.BW_ACCOUNT_ID]
-      }, {
-        ApplicationId: [process.env.BW_APPLICATION_ID]
-      }, {
-        Action: ["Authentication"]
-      }, {
-        To: [{
-          PhoneNum: [process.env.TO_PHONENUMBER]
-        }]
-      }, {
-        From: [{
-          PhoneNum: [process.env.BW_APPLICATION_PHONENUMBER]
-        }]
-      }]
-    }, {
-      auth: {
-        username: process.env.BW_API_USER,
-        password: process.env.BW_API_PASS
-      }
-    });
+  return await mfa.APIController.createMessagingTwoFactor(process.env.BW_ACCOUNT_ID,
+    new twofactor.TwoFactorCodeRequestSchema({
+      from: process.env.BW_APPLICATION_PHONENUMBER,
+      to: process.env.TO_PHONENUMBER,
+      applicationId: process.env.BW_APPLICATION_ID,
+      scope: 'authorization'
+    }));
 };
 
 const requestTwoFactorVerification = async (pin) => {
-  return axios
-    .post(process.env.BW_TWOFACTOR_URL, {
-      CheckCode: [{
-        AccountId: [process.env.BW_ACCOUNT_ID]
-      }, {
-        ApplicationId: [process.env.BW_APPLICATION_ID]
-      }, {
-        Action: ["Authentication"]
-      }, {
-        To: [{
-          PhoneNum: [process.env.TO_PHONENUMBER]
-        }]
-      }, {
-        From: [{
-          PhoneNum: [process.env.BW_APPLICATION_PHONENUMBER]
-        }]
-      }, pin + ""]
-    }, {
-      auth: {
-        username: process.env.BW_API_USER,
-        password: process.env.BW_API_PASS
-      }
-    }).then(res =>
-      res.data
-    );
+  return await mfa.APIController.createVerifyTwoFactor(process.env.BW_ACCOUNT_ID,
+    new twofactor.TwoFactorVerifyRequestSchema({
+      from: process.env.BW_APPLICATION_PHONENUMBER,
+      to: process.env.TO_PHONENUMBER,
+      applicationId: process.env.BW_APPLICATION_ID,
+      scope: 'authorization',
+      code: pin
+    }));
 };
 
 module.exports = {
